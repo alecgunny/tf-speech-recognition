752e790cef40dd5328fd32e58e6580678a7cc91d
Alec MacLean Gunny <agunny@nvidia.com>
2018-10-09 12:33:34 -0700

    rebuilt to use container as executable and contain all the scripts inside of it

--- launch.sh
+++ launch.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
-SHORT_OPTS="d:ph"
-LONG_OPTS="data_dir:,preproc,help"
+SHORT_OPTS="d:k:h"
+LONG_OPTS="data_dir:,kaggle:,help"
 OPTS=`getopt -o $SHORT_OPTS --long $LONG_OPTS -n 'parse-options' -- "$@"`
 if [ $? != 0 ] ; then echo "Failed parsing options." >&2 ; exit 1 ; fi
 eval set -- "$OPTS"
 
-CMD="jupyter notebook --ip=0.0.0.0"
+CMD="jupyter notebook --allow-root --ip=0.0.0.0"
+KAGGLE_CONFIG_DIR=$PWD/.kaggle
 while true; do
   case "$1" in
-    -d | --data_dir ) DATA_DIR="$2"; shift; shift ;;
-    -p | --preproc  ) CMD="preproc/preproc.sh"; shift ;;
-    -h | --help     ) HELP=true; shift ;;
+    -d | --data_dir   ) DATA_DIR="$2"; shift; shift ;;
+    -k | --kaggle     ) KAGGLE_CONFIG_DIR="$2"; shift; shift ;;
+    -h | --help       ) HELP=true; shift ;;
     -- ) shift; break ;;
     * ) break ;;
   esac
@@ -21,7 +22,7 @@ if ! [ -z "$HELP" ]; then
   echo "Parameters"
   echo "----------"
   echo "    -d, --data_dir  : path to data (or where to save data if running preprocessing)"
-  echo "    -p, --preproc   : whether to run preprocessing data generation script. If set, notebook won't launch"
+  echo "    -k, --kaggle    : path to directory containing kaggle API key JSON"
   echo "    -h, --help      : show this help"
   exit 0
 fi
@@ -36,10 +37,7 @@ docker run \
   -it \
   --runtime=nvidia \
   -v $DATA_DIR:/data \
-  -v $PWD:/workspace \
-  --workdir /workspace \
+  -v $KAGGLE_CONFIG_DIR:/workspace/.kaggle/ \
   -p 8888:8888 \
   -p 6006:6006 \
-  -u $(id -u):$(id -g) \
-  $USER/tf-src \
-  $CMD
+  $USER/tf-src:allin
